ac744b5d5f471af1563fc08abf47430bb2cb2ece
DorinVieru <dorino.vieru@gmail.com>
2024-02-12 17:01:33 +0100

    [UP] Aggiunta di immagini e creazione dell'header

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/comics', function () {
+    return view('comics');
+})->name('comics');
+
+Route::get('/characters', function () {
+    return view('characters');
+})->name('characters');
