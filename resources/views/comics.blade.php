6fe6016ff241f0eec00d3800857a00f355b0a419
DorinVieru <dorino.vieru@gmail.com>
2024-02-12 18:31:47 +0100

    [UP] Inserita sezione bg-blue con le icone

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -27,6 +27,54 @@
         </div>
     </main>
 
+    {{-- SECTION BG-BLUE --}}
+    <div class="container-fluid py-5 bg-blu-icon">
+        <div class="container">
+            <div class="row justify-content-around">
+                <div class="col-2 d-flex align-items-center px-0">
+                    <div>
+                        <img class="img-main" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    </div> 
+                    <div>
+                        <h6>DIGITAL COMICS</h6>
+                    </div>
+                </div>
+                <div class="col-2 d-flex align-items-center px-0">
+                    <div>
+                        <img class="img-main" src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    </div> 
+                    <div>
+                        <h6>DC MERCHANDISE</h6>
+                    </div>
+                </div>
+                <div class="col-2 d-flex align-items-center px-0">
+                    <div>
+                        <img class="img-main" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    </div> 
+                    <div>
+                        <h6>SUBSCRIPTION</h6>
+                    </div>
+                </div>
+                <div class="col-2 d-flex align-items-center px-0">
+                    <div>
+                        <img class="img-main" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    </div> 
+                    <div>
+                        <h6>COMIC SHOP LOCATOR</h6>
+                    </div>
+                </div>
+                <div class="col-2 d-flex align-items-center px-0">
+                    <div>
+                        <img class="img-main" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    </div> 
+                    <div>
+                        <h6>DC POWER VISA</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     {{-- FOOTER --}}
     @include('partials.footer')
 </body>
